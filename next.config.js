4da7699bfe4a0ad7e6202e4a147cb4f5e0374c76
Pliskeviski <gustavopliskeviski@gmail.com>
2021-11-24 12:27:39 -0300

    feat(lint): configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
+const withImages = require('next-images');
+const withFonts = require('next-fonts');
+
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+module.exports = withImages(
+  withFonts({
+    projectRoot: __dirname,
+    reactStrictMode: true,
+    devIndicators: {
+      autoPrerender: false,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: `${process.env.API_URL}/:path*`,
+        },
+      ];
+    },
+  })
+);
